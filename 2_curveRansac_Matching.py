53bb568c10d646f89c3bab58674b0f1574a1d8cb
Young Il PARK <73331241+ChicagoPark@users.noreply.github.com>
2022-05-17 16:54:47 +0900

    Add files via upload

--- 2_curveRansac_Matching.py
+++ 2_curveRansac_Matching.py
@@ -41,6 +41,8 @@ def ransac_plot(xArray, yArray, y_hat, inlier_mask):
     plt.ylabel("Response")
     plt.show()
 
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 sn = int(sys.argv[1]) if len(sys.argv)>1 else 7 #default 7
 name = '%06d'%sn # 6 digit zeropadding
@@ -100,13 +102,14 @@ scan = planePCDarray
 points = scan[:, 0:3] # lidar xyz (front, left, up)
 points_with_intensity = scan[:, 0:4]
 
-print(points.shape)
 # TODO: use fov filter? 
 velo = np.insert(points,3,1,axis=1).T
 velo_with_intensity = np.insert(points_with_intensity,4,1,axis=1).T
 
 velo = np.delete(velo,np.where(velo[0,:]<0),axis=1)
 cam = P2 * R0_rect * Tr_velo_to_cam * velo
+
+
 #cam = np.delete(cam,np.where(cam[2,:]<0)[1],axis=1)
 new_cam = np.transpose(cam)
 new_velo = np.transpose(velo)
@@ -115,7 +118,6 @@ new_velo_with_intensity = np.transpose(velo_with_intensity)
 new_veloList = list(np.squeeze(np.asarray(new_velo)))
 new_veloList_with_intensity = list(np.squeeze(np.asarray(new_velo_with_intensity)))
 new_camList = list(np.squeeze(np.asarray(new_cam)))
-print("YOUNGIL")
 
 index = 0
 
@@ -153,8 +155,8 @@ for i in range(len(new_veloList)):
             #if int(project2Dx)==int(rightlaneX[j]) and int(project2Dy)==int(rightlaneY[j]):
                 rightlane3D.append([veloList[0], veloList[1], veloList[2]])
 
-print(f"overlapped(left): {len(leftlane3D)}")
-print(f"overlapped(right): {len(rightlane3D)}")
+#print(f"overlapped(left): {len(leftlane3D)}")
+#print(f"overlapped(right): {len(rightlane3D)}")
 
 leftlane3D = sorted(leftlane3D)
 rightlane3D = sorted(rightlane3D)
@@ -181,21 +183,16 @@ rightMinY = min(rightlane3D[0][1], rightlane3D[-1][1])
 rightMaxX = rightlane3D[-1][0]
 rightMaxY = max(rightlane3D[0][1], rightlane3D[-1][1])
 
-print("----YOUGNIL TEXT")
-
-print(leftMaxX)
-print(rightMaxX)
-
-print("YOUGNIL TEXT----")
-
 leftPointinRange = []
 rightPointinRange = []
 
 # instant variables
 leftXXX = []
 leftYYY = []
+leftZZZ = []
 rightXXX = []
 rightYYY = []
+rightZZZ = []
 
 # set the extra range as 2 meters
 extraRange = 0.5 
@@ -217,21 +214,111 @@ for i in range(len(new_veloList_with_intensity)):
 
     if veloList[0] >= -2:
         # left lane process
-        if veloList[0] > leftMinX-extraRange and veloList[0] < leftMaxX+extraRange+20 and veloList[1] > leftMinY -extraRange and veloList[1] < leftMaxY + extraRange and veloList[3]>0.4:
+        if veloList[0] > leftMinX-extraRange and veloList[0] < leftMaxX+extraRange+20 and veloList[1] > leftMinY -extraRange and veloList[1] < leftMaxY + extraRange and veloList[3]>0.47:
             leftPointinRange.append([veloList[0], veloList[1], veloList[2]])
             leftXXX.append(veloList[0])
             leftYYY.append(veloList[1])
+            leftZZZ.append(veloList[2])
         
         # right lane process
-        if veloList[0] > rightMinX-extraRange and veloList[0] < rightMaxX+extraRange+20 and veloList[1] > rightMinY-extraRange and veloList[1] < rightMaxY+extraRange and veloList[3]>0.4:
+        if veloList[0] > rightMinX-extraRange and veloList[0] < rightMaxX+extraRange+20 and veloList[1] > rightMinY-extraRange and veloList[1] < rightMaxY+extraRange and veloList[3]>0.47:
             rightPointinRange.append([veloList[0], veloList[1], veloList[2]])
             rightXXX.append(veloList[0])
             rightYYY.append(veloList[1])
+            rightZZZ.append(veloList[2])
+
+# image matching process
+left_img_point_cloud = []
+right_img_point_cloud = []
+
+# (1) generate point numpy
+for i in range(len(leftXXX)):
+    left_img_point_cloud.append([leftXXX[i], leftYYY[i], leftZZZ[i]])
+
+for i in range(len(rightXXX)):
+    right_img_point_cloud.append([rightXXX[i], rightYYY[i], rightZZZ[i]])
+
+left_img_point_cloud = np.array(left_img_point_cloud)
+right_img_point_cloud = np.array(right_img_point_cloud)
+
+#print(left_img_point_cloud.shape)
+#print(right_img_point_cloud.shape)
+
+left_velo = np.insert(left_img_point_cloud, 3, 1, axis=1).T
+right_velo = np.insert(right_img_point_cloud, 3, 1, axis=1).T
+
+left_cam = P2 * R0_rect * Tr_velo_to_cam * left_velo
+right_cam = P2 * R0_rect * Tr_velo_to_cam * right_velo
+
+left_new_cam = np.transpose(left_cam)
+left_new_velo = np.transpose(left_velo)
+
+right_new_cam = np.transpose(right_cam)
+right_new_velo = np.transpose(right_velo)
+
+left_new_camList = list(np.squeeze(np.asarray(left_new_cam)))
+left_new_veloList = list(np.squeeze(np.asarray(left_new_velo)))
+right_new_camList = list(np.squeeze(np.asarray(right_new_cam)))
+right_new_veloList = list(np.squeeze(np.asarray(right_new_velo)))
+
+png = mpimg.imread(img)
+gray = rgb2gray(png).transpose()
+
+img_intensity_lx = []
+img_intensity_ly = []
+img_intensity_rx = []
+img_intensity_ry = []
+
+intensity_threadhold = 0.35
+for i in range(len(left_new_veloList)):
+    onecamList = list(np.squeeze(np.asarray(left_new_camList[i]).reshape(-1)))
+
+    project2Dx= onecamList[0]/onecamList[2]
+    project2Dy = onecamList[1]/onecamList[2]
+    
+    first_pixel = [math.floor(project2Dx), math.floor(project2Dy)]
+    second_pixel = [math.ceil(project2Dx), math.ceil(project2Dy)]
+    third_pixel = [math.floor(project2Dx), math.ceil(project2Dy)]
+    fourth_pixel = [math.ceil(project2Dx), math.floor(project2Dy)]
+
+    # [1] access to the points on the image array that are matched with inlier points
+    if first_pixel[1]>375 or second_pixel[1]>375 or third_pixel[1]>375 or fourth_pixel[1]>375:
+        continue
+    # [2] get the pixel values of the above and compare it with threshold value
+    if gray[first_pixel[0]][first_pixel[1]] >= intensity_threadhold or gray[second_pixel[0]][second_pixel[1]] >= intensity_threadhold or gray[third_pixel[0]][third_pixel[1]] >= intensity_threadhold or gray[fourth_pixel[0]][fourth_pixel[1]] >= intensity_threadhold:
+        img_intensity_lx.append(project2Dx)
+        img_intensity_ly.append(project2Dy)
+
+for i in range(len(right_new_veloList)):
+    onecamList = list(np.squeeze(np.asarray(right_new_camList[i]).reshape(-1)))
+
+    project2Dx= onecamList[0]/onecamList[2]
+    project2Dy = onecamList[1]/onecamList[2]
+    
+    first_pixel = [math.floor(project2Dx), math.floor(project2Dy)]
+    second_pixel = [math.ceil(project2Dx), math.ceil(project2Dy)]
+    third_pixel = [math.floor(project2Dx), math.ceil(project2Dy)]
+    fourth_pixel = [math.ceil(project2Dx), math.floor(project2Dy)]
+
+    # [1] access to the points on the image array that are matched with inlier points
+    if first_pixel[1]>374 or second_pixel[1]>374 or third_pixel[1]>374 or fourth_pixel[1]>374 or first_pixel[0]>1240 or second_pixel[0]>1240 or third_pixel[0]>1240 or fourth_pixel[0]>1240:
+        continue
+    # [2] get the pixel values of the above and compare it with threshold value
+    if gray[first_pixel[0]][first_pixel[1]] >= intensity_threadhold or gray[second_pixel[0]][second_pixel[1]] >= intensity_threadhold or gray[third_pixel[0]][third_pixel[1]] >= intensity_threadhold or gray[fourth_pixel[0]][fourth_pixel[1]] >= intensity_threadhold:
+        img_intensity_rx.append(project2Dx)
+        img_intensity_ry.append(project2Dy)
+
+#--------
 
 leftX_RansacData = np.array(leftXXX).reshape(-1,1)
 leftY_RansacData = np.array(leftYYY).reshape(-1,1)
 rightX_RansacData = np.array(rightXXX).reshape(-1,1)
 rightY_RansacData = np.array(rightYYY).reshape(-1,1)
+
+#leftXXX.insert(0, leftXXX[0]);leftXXX.insert(0,leftXXX[0]);leftXXX.insert(0,leftXXX[0]); leftYYY.insert(0,leftYYY[0]+0.01);leftYYY.insert(0, leftYYY[0]+0.015);leftYYY.insert(0,leftYYY[0]-0.001)
+#rightXXX.insert(0, rightXXX[0]);rightXXX.insert(0,rightXXX[0]);rightXXX.insert(0,rightXXX[0]); rightYYY.insert(0,rightYYY[0]);rightYYY.insert(0, rightYYY[0]);rightYYY.insert(0,rightYYY[0]); rightXXX.insert(0, rightXXX[0]);rightXXX.insert(0,rightXXX[0]);rightXXX.insert(0,rightXXX[0]); rightYYY.insert(0,rightYYY[0]);rightYYY.insert(0, rightYYY[0]);rightYYY.insert(0,rightYYY[0]);rightXXX.insert(0, rightXXX[0]);rightXXX.insert(0,rightXXX[0]);rightXXX.insert(0,rightXXX[0]); rightYYY.insert(0,rightYYY[0]);rightYYY.insert(0, rightYYY[0]);rightYYY.insert(0,rightYYY[0]); rightXXX.insert(0, rightXXX[0]);rightXXX.insert(0,rightXXX[0]);rightXXX.insert(0,rightXXX[0]); rightYYY.insert(0,rightYYY[0]);rightYYY.insert(0, rightYYY[0]);rightYYY.insert(0,rightYYY[0])
+#leftXXX.append(leftXXX[0]);leftXXX.append(leftXXX[0]);leftXXX.append(leftXXX[0]); leftYYY.append(leftYYY[0]+0.01);leftYYY.append(leftYYY[0]+0.015);leftYYY.append(leftYYY[0]-0.001)
+
 
 # Ransac data preprocessing
 test_leftX = np.array(leftXXX)
@@ -264,6 +351,9 @@ cam[:2] /= cam[2,:]
 plt.figure(figsize=(12,5),dpi=96,tight_layout=True)
 png = mpimg.imread(img)
 IMG_H,IMG_W,_ = png.shape
+
+plt.plot(img_intensity_lx, img_intensity_ly, color="blue", linewidth=3)
+plt.plot(img_intensity_rx, img_intensity_ry, color="blue", linewidth=3)
 
 # restrict canvas in range
 plt.axis([0,IMG_W,IMG_H,0])
